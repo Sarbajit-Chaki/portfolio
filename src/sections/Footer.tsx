4f96627863b46646c0e706e89f533380ac9341b8
Sarbajit-Chaki <159568844+Sarbajit-Chaki@users.noreply.github.com>
2026-01-06 22:49:58 +0530

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,7 +3,7 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 const footerLinks = [
   {
     title: 'Linkedin',
-    href: 'https://www.linkedin.com/in/sarbajit-chaki/',
+    href: 'https://www.linkedin.com/in/sarbajit-chaki-dev/',
   },
   {
     title: 'Facebook',
